cb95edf5ba2926cd6bd30ae2cee818ad255f97b1
hjariwala25 <hjariwala150@gmail.com>
2024-10-11 12:00:31 +0530

    fix : localstorage error

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,10 +1,10 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid"
-import { Prev } from "react-bootstrap/esm/PageItem";
+ 
 
 
 type NoteFormProps = {
@@ -17,14 +17,17 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault()
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: [],
+            tags: selectedTags,
         })
+
+        navigate("..")
     }
     return (
         <Form onSubmit={handleSubmit}>
