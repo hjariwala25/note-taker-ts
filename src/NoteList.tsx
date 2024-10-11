d334abd94baa3380be88370772fdc2c4e52aa5a6
hjariwala25 <hjariwala150@gmail.com>
2024-10-11 13:56:58 +0530

    add : notelist

---
+++ src/NoteList.tsx
@@ -0,0 +1,73 @@
+import { useMemo, useState } from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Note, Tag } from "./App";
+
+type NoteListProps = {
+    availableTags: Tag[]
+    notes: Note[]
+}
+
+export function NoteList({ availableTags, notes }: NoteListProps) {
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const [title, setTitle] = useState("")
+
+    const filterNotes = useMemo(() => {
+        return notes.filter(note => {
+            return ((title === "" || note.title.toLowerCase().includes(title.toLowerCase())) && (selectedTags.length === 0 || selectedTags.every(tag => note.tags.some(noteTag => noteTag.id === tag.id))))
+        })
+    }, [title, selectedTags, notes])
+
+    return <>
+        <Row className="aligh-item-center mb-4">
+            <Col>
+                <h1>Notes</h1>
+            </Col>
+            <Col xs="auto">
+                <Stack gap={2} direction="horizontal">
+                    <Link to="/new">
+                        <Button variant="primary">Create</Button>
+                    </Link>
+                    <Button variant="outline-secondary">Edit Tags</Button>
+                </Stack>
+            </Col>
+        </Row>
+        <Form>
+            <Row className="mb-4">
+                <Col>
+                    <Form.Group controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" value={title} onChange={e => setTitle(e.target.value)} />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="tags">
+                        <Form.Label>Tags</Form.Label>
+                        <ReactSelect
+
+                            value={selectedTags.map(tags => {
+                                return { label: tags.label, value: tags.id }
+                            })}
+                            options={availableTags.map(tag => {
+                                return { label: tag.label, value: tag.id }
+                            })}
+                            onChange={tags => {
+                                setSelectedTags(tags.map(tag => {
+                                    return ({ label: tag.label, id: tag.value })
+                                }))
+                            }}
+                            isMulti />
+                    </Form.Group>
+                </Col>
+            </Row>
+        </Form>
+        <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+            {filterNotes.map(note => (
+                <Col key={note.id}>
+                   
+                </Col>
+            ))}
+        </Row>
+    </>
+}
